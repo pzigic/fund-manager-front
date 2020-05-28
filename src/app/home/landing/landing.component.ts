95a0b8dd07f57483e4537062068c734b2d7d2f0b
Petar <zigic@outlook.com>
2020-05-28 10:54:24 -0500

    ViewChild compatibility with Angular 8

--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -18,8 +18,8 @@ export class LandingComponent implements OnInit, AfterViewInit {
 
   isLoadingResults = true;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(private service : LandingService) { }
 
